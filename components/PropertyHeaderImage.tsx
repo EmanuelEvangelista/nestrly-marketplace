808e4ef72c8ee8edf8e1e697c64825df53d4ffea
Emanuel Evangelista <emanu.vange@gmail.com>
2026-01-21 23:47:54 -0300

    feat: PropertiHeaderImage with details properties

---
+++ components/PropertyHeaderImage.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Image from "next/image";
+
+interface PropertyHeaderImageProps {
+  image: string;
+  name: string;
+}
+
+const PropertyHeaderImage = ({ image, name }: PropertyHeaderImageProps) => {
+  return (
+    <section>
+      <div className="container-xl m-auto">
+        <div className="grid grid-cols-1">
+          <Image
+            src={`/images/properties/${image}`}
+            alt={name}
+            className="object-cover w-full h-96 rounded-b-lg"
+            width={0}
+            height={0}
+            sizes="100vw"
+            priority={true}
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default PropertyHeaderImage;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./pages/**/*.{js,jsx}",
-    "./components/**/*.{js,jsx}",
-    "./app/**/*.{js,jsx}",
-    "./src/**/*.{js,jsx}",
-  ],
+  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ["Poppins", "sans-serif"],
+    },
+    extend: {
+      gridTemplateColumns: {
+        "70/30": "70% 30%",
+      },
+      colors: {
+        brandRed: "#e63946",
+        brandBlue: "#1d3557",
+      },
+    },
   },
   plugins: [],
 };
